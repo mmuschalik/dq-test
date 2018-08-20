c87af45a060409bca9b5487cdfa761cf39a14c1f
Maurice <maurice@actio.com.au>
2018-08-20 21:22:38 +1000

    fixed backwards compatibility

--- dfunc.py
+++ dfunc.py
@@ -4,22 +4,18 @@ import rule_functions as rf
 
 def getRuleLambda(rule):
     res = []
-    funcName = rule['type']
+    funcName = rule['type'] if 'type' in rule else 'udf'
     for key, value in rule.items():
         if key not in ['type','description','dataset', 'points', 'keywords','filter']:
             res.append(key + '=' + ("'" + value + "'" if isinstance(value, str) else str(value)))
-    print('lambda d: rf.' + funcName + '(data=d, ' + ', '.join(res) + ')')
+    
     return eval('lambda d: rf.' + funcName + '(data=d, ' + ', '.join(res) + ')')
 
 def apply(data, expr):
     return data.apply(eval("lambda " + expr), axis=1)
 
 def filter(data, expr):
-    data_copy = data.copy()
-    return data_copy[eval("lambda " + expr.replace('->',': '))(data_copy)]
-
-
-
+    return data[eval("lambda " + expr.replace('->',': '))(data)]
 
 
 
---
+++ dq2.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import dfunc as df
+import numpy as np
+import yaml
+from yaml import load, dump
+
+
+class dq:
+
+    yml = None
+    dset = None
+    data = pd.DataFrame()
+
+    def loadYmlFile(self, path):
+        stream = open(path, 'r')
+        self.yml = load(stream)
+        return self.yml
+    
+    def loadYml(self, yml):
+        self.yml = {**self.yml, **yml}
+    
+    def datasource(self, name):
+        return self.yml['datasource'][name]
+
+    def dataset(self, name):
+        return self.yml['dataset'][name]
+
+    def rule(self, name):
+        res = self.yml['analysis'][name]
+
+        if 'function' in res:
+            res['function'] = self.yml['function'][res['function']]
+        return res
+
+    def extractDataset(self, dataset_name):
+        self.dset = self.dataset(dataset_name)
+        self.data = pd.read_csv(self.datasource(self.dset['datasource'])['url'], index_col=False, parse_dates=[0])
+        return self.data
+
+    def showFails(self, rule_name):
+        ret = self.executeRule(rule_name)
+        return ret[ret.value == 0]
+    
+    def executeRule(self, rule_name):
+        rule = self.rule(rule_name)
+        r = df.getRuleLambda(rule)
+        
+        if 'filter' in rule:
+            input = df.filter(self.data, rule['filter'])
+        else:
+            input = self.data
+
+        res = r(input)
+        update_to_list = lambda a: a if isinstance(a, list) else [a]
+        res['source_id'] = eval("+ ', ' +".join(map(lambda c: "input['" + c + "'].map(str)", update_to_list(self.dset['id']))))
+        return res
+
+
+mydq = dq()
+yml = mydq.loadYmlFile('test.yml')
+mydq.extractDataset('titanic')
+print(mydq.executeRule('a1'))
+
