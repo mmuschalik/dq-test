a1dc4ef22a800143bb49200042eea9b847a402f8
Maurice <maurice@actio.com.au>
2018-08-30 00:22:13 +1000

    add logging

--- exec_yml.bat
+++ exec_yml.bat
@@ -1,3 +1,3 @@
 del .\data\*.csv
-python exec_yml_to_csv.py %1
-for %%s in (.\data\*) do bcp  mydb.dbo.row_analysis_history IN %%s -f dq_result.fmt -T -F2
+python exec_yml_to_csv.py %*
+for %%s in (.\data\*) do bcp  mydb.dbo.row_analysis_history IN "%%s" -f dq_result.fmt -T -F2 -S localhost
--- exec_yml_to_csv.py
+++ exec_yml_to_csv.py
@@ -3,7 +3,12 @@ import csv
 import sys
 import os
 import datetime
+import logging
+import logging.config
 from sqlalchemy import create_engine
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 paths = sys.argv[1:]
 
@@ -22,13 +27,30 @@ conn.execute("insert into dbo.run (description) values (null)")
 runid = conn.execute("select max(id) as runid from dbo.run").fetchone()['runid']
 
 for dkey, dvalue in yml['dataset'].items():
-    print('Extracting dataset: ' + dkey)
-    analysis.extractDataset(dkey)
-    for rkey, rvalue in yml['analysis'].items():
-        if(yml['analysis'][rkey]['dataset'] == dkey):
-            res = analysis.executeRule(rkey, True)
-            res['execution_id'] = "RUN" + str(runid)
-            res['execution_date'] = datetime.datetime.now().strftime('%Y-%m-%d')
-            print('Saving results for rule: ' + rkey + '...')
-            res.to_csv('data/' + dkey + '_' + rkey + '.csv', sep='\t', index=False,  quoting=csv.QUOTE_NONE)
-            print('Results saved for rule: ' + rkey)
+
+    try:
+
+        logger.info('Extracting dataset: ' + dkey + '...')
+        analysis.extractDataset(dkey)
+        logger.info('Finshed extracting dataset: ' + dkey)
+
+        for rkey, rvalue in yml['analysis'].items():
+            if(yml['analysis'][rkey]['dataset'] == dkey):
+
+                try:
+
+                    logger.info('Executing Rule: ' + rkey + '...')
+                    res = analysis.executeRule(rkey, True)
+                    logger.info('Finished executing Rule: ' + rkey)
+
+                    res['execution_id'] = "RUN" + str(runid)
+                    res['execution_date'] = datetime.datetime.now().strftime('%Y-%m-%d')
+
+                    logger.info('Saving results for rule: ' + rkey + '...')
+                    res.to_csv('data/' + dkey + '_' + rkey + '.csv', sep='\t', index=False, escapechar='\\',  quoting=csv.QUOTE_NONE)
+                    logger.info('Results saved for rule: ' + rkey)
+    
+                except Exception:
+                    pass
+    except Exception:
+        pass
