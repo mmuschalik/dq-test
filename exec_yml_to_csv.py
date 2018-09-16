bfc99b673d961985b1d07e68a7ac09666bf75e67
mmuschalik <maurice.muschalik@gmail.com>
2018-09-16 22:33:36 +1000

    improve logging

--- exec_yml_to_csv.py
+++ exec_yml_to_csv.py
@@ -5,10 +5,19 @@ import os
 import datetime
 import logging
 import logging.config
+import yaml
 from sqlalchemy import create_engine
 
-logging.basicConfig(level=logging.INFO)
+# prepare logger
+if os.path.exists('logger.yaml'):
+    with open('logger.yaml', 'rt') as f:
+        config = yaml.safe_load(f.read())
+    logging.config.dictConfig(config)
+else:
+    logging.basicConfig(level=logging.INFO)
+
 logger = logging.getLogger(__name__)
+
 
 paths = sys.argv[1:]
 
@@ -20,11 +29,14 @@ for path in paths:
 yml = analysis.yml
 prefix = os.path.basename(path).replace('.yml','')
 
-
+# get new run from db
 engine = create_engine(yml['datasource']['Result']['url'])
 conn = engine.connect()
 conn.execute("insert into run (description) values (null)")
 runid = conn.execute("select max(id) as runid from run").fetchone()['runid']
+
+logger.info('----------------------------------------')
+logger.info('Starting run: RUN' + str(runid) + '...')
 
 for dkey, dvalue in yml['dataset'].items():
 
@@ -46,7 +58,7 @@ for dkey, dvalue in yml['dataset'].items():
                     res['execution_id'] = "RUN" + str(runid)
                     res['execution_date'] = datetime.datetime.now().strftime('%Y-%m-%d')
 
-                    logger.info('Saving results for rule: ' + rkey + '...')
+                    logger.info('Saving results for rule: ' + rkey + ' at  + ./data/' + dkey + '_' + rkey + '.csv')
                     res.to_csv('data/' + dkey + '_' + rkey + '.csv', sep='\t', index=False, escapechar='\\',  quoting=csv.QUOTE_NONE)
                     logger.info('Results saved for rule: ' + rkey)
     
@@ -56,3 +68,5 @@ for dkey, dvalue in yml['dataset'].items():
     except:
         traceback.print_exc()
         pass
+
+logger.info('Run completed: RUN' + str(runid))
